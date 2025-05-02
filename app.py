d40592f6eda0551ce1567b474a8c1bc8c8dba26a
Santi <zopotamadre290@gmail.com>
2025-05-02 06:58:58 -0500

    App Factorial funcional

--- app.py
+++ app.py
@@ -1,1 +1,17 @@
- 
+from flask import Flask, Response
+import math
+
+app = Flask(__name__)
+
+@app.route('/factorial/<int:num>') # http://127.0.0.1:5000/ 
+def getFactorial(num):
+    try:
+        res = math.factorial(num) #res = Resultado
+        msj = f"El factorial de {num} es {res}" #msj = Mensaje
+    except ValueError:
+        msj = "Error: Numero no valido"
+    
+    return Response(msj, mimetype='text/plain')
+
+if __name__ == '__main__':
+    app.run(debug=True)
